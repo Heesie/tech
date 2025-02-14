cf3d51d835ab7b5f7dd6233fa7004d8bf07e3448
Heesie <ljvanhees@outlook.com>
2025-02-14 14:03:23 +0100

    views

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ const app = express()
 app 
 .use('/static', express.static('static'))
 
+.set('view engine', 'ejs')
+.set('views', 'views')
 .get ('/', onhome)
 .get ('/about', onabout)
+.get ('/movie', movie)
 
 .listen(8000)
+
+
 
 function onhome(req, res) {
     res.send('<h1>Hello World</h1>')
@@ -15,4 +20,12 @@ function onhome(req, res) {
 
 function onabout(req, res) {
     res.send('<h1>About me</h1>')
+}
+
+function movie(req, res, next) {
+    let movie = {
+        title: 'the shawshank redemtion',
+        description: 'Andy Dufresne is young and..'
+    }
+    res.render('details.ejs', {data: movie})
 }
